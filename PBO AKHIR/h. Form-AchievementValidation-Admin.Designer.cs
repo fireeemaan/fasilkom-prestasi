df416779d43e0dc9898091aa4cafd1355b05d772
areka06 <116472942+areka06@users.noreply.github.com>
2023-12-02 04:06:24 +0700

    Update Form Status Validation

--- PBO AKHIR/h. Form-AchievementValidation-Admin.Designer.cs
+++ PBO AKHIR/h. Form-AchievementValidation-Admin.Designer.cs
@@ -29,51 +29,92 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_AchievementValidation_Admin));
-            DataValid = new RadioButton();
-            DataInvalid = new RadioButton();
             textBox5 = new TextBox();
-            kryptonDataGridView1 = new Krypton.Toolkit.KryptonDataGridView();
-            ((System.ComponentModel.ISupportInitialize)kryptonDataGridView1).BeginInit();
+            DataInvalid = new RadioButton();
+            DataValid = new RadioButton();
+            tbxNamaLomba = new TextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox6 = new TextBox();
             SuspendLayout();
-            // 
-            // DataValid
-            // 
-            DataValid.AutoSize = true;
-            DataValid.Location = new Point(512, 624);
-            DataValid.Name = "DataValid";
-            DataValid.Size = new Size(17, 16);
-            DataValid.TabIndex = 0;
-            DataValid.TabStop = true;
-            DataValid.UseVisualStyleBackColor = true;
-            // 
-            // DataInvalid
-            // 
-            DataInvalid.AutoSize = true;
-            DataInvalid.Location = new Point(658, 624);
-            DataInvalid.Name = "DataInvalid";
-            DataInvalid.Size = new Size(17, 16);
-            DataInvalid.TabIndex = 1;
-            DataInvalid.TabStop = true;
-            DataInvalid.UseVisualStyleBackColor = true;
             // 
             // textBox5
             // 
             textBox5.BorderStyle = BorderStyle.None;
-            textBox5.Location = new Point(426, 698);
+            textBox5.Location = new Point(426, 327);
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(993, 20);
-            textBox5.TabIndex = 5;
+            textBox5.TabIndex = 8;
             // 
-            // kryptonDataGridView1
+            // DataInvalid
             // 
-            kryptonDataGridView1.BorderStyle = BorderStyle.None;
-            kryptonDataGridView1.ColumnHeadersHeight = 36;
-            kryptonDataGridView1.Location = new Point(338, 227);
-            kryptonDataGridView1.Name = "kryptonDataGridView1";
-            kryptonDataGridView1.RowHeadersWidth = 51;
-            kryptonDataGridView1.RowTemplate.Height = 29;
-            kryptonDataGridView1.Size = new Size(1170, 340);
-            kryptonDataGridView1.TabIndex = 6;
+            DataInvalid.AutoSize = true;
+            DataInvalid.Location = new Point(658, 251);
+            DataInvalid.Name = "DataInvalid";
+            DataInvalid.Size = new Size(17, 16);
+            DataInvalid.TabIndex = 7;
+            DataInvalid.TabStop = true;
+            DataInvalid.UseVisualStyleBackColor = true;
+            // 
+            // DataValid
+            // 
+            DataValid.AutoSize = true;
+            DataValid.Location = new Point(512, 251);
+            DataValid.Name = "DataValid";
+            DataValid.Size = new Size(17, 16);
+            DataValid.TabIndex = 6;
+            DataValid.TabStop = true;
+            DataValid.UseVisualStyleBackColor = true;
+            // 
+            // tbxNamaLomba
+            // 
+            tbxNamaLomba.BorderStyle = BorderStyle.None;
+            tbxNamaLomba.Location = new Point(404, 419);
+            tbxNamaLomba.Name = "tbxNamaLomba";
+            tbxNamaLomba.Size = new Size(483, 20);
+            tbxNamaLomba.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(959, 419);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(483, 20);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Location = new Point(959, 516);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(483, 20);
+            textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Location = new Point(404, 516);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(483, 20);
+            textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            textBox4.BorderStyle = BorderStyle.None;
+            textBox4.Location = new Point(959, 613);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(483, 20);
+            textBox4.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            textBox6.BorderStyle = BorderStyle.None;
+            textBox6.Location = new Point(404, 613);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(483, 20);
+            textBox6.TabIndex = 13;
             // 
             // Form_AchievementValidation_Admin
             // 
@@ -81,23 +122,32 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(1582, 853);
-            Controls.Add(kryptonDataGridView1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox6);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(tbxNamaLomba);
             Controls.Add(textBox5);
             Controls.Add(DataInvalid);
             Controls.Add(DataValid);
             Name = "Form_AchievementValidation_Admin";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form_AchievementValidation_Admin";
-            ((System.ComponentModel.ISupportInitialize)kryptonDataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private RadioButton DataValid;
-        private RadioButton DataInvalid;
         private TextBox textBox5;
-        private Krypton.Toolkit.KryptonDataGridView kryptonDataGridView1;
+        private RadioButton DataInvalid;
+        private RadioButton DataValid;
+        private TextBox tbxNamaLomba;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox6;
     }
 }
