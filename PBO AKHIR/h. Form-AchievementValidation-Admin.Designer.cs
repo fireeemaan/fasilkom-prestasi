0aedf864c99b81693f4b3012debbd7f9067adf53
fireeemaan <firmancakep222@gmail.com>
2023-12-02 12:46:44 +0700

    .

--- PBO AKHIR/h. Form-AchievementValidation-Admin.Designer.cs
+++ PBO AKHIR/h. Form-AchievementValidation-Admin.Designer.cs
@@ -30,8 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_AchievementValidation_Admin));
             textBox5 = new TextBox();
-            DataInvalid = new RadioButton();
-            DataValid = new RadioButton();
+            rbtnInvalid = new RadioButton();
+            rbtnValid = new RadioButton();
             tbxNamaLomba = new TextBox();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
@@ -48,31 +48,32 @@
             textBox5.Size = new Size(993, 20);
             textBox5.TabIndex = 8;
             // 
-            // DataInvalid
+            // rbtnInvalid
             // 
-            DataInvalid.AutoSize = true;
-            DataInvalid.Location = new Point(658, 251);
-            DataInvalid.Name = "DataInvalid";
-            DataInvalid.Size = new Size(17, 16);
-            DataInvalid.TabIndex = 7;
-            DataInvalid.TabStop = true;
-            DataInvalid.UseVisualStyleBackColor = true;
+            rbtnInvalid.AutoSize = true;
+            rbtnInvalid.Location = new Point(658, 251);
+            rbtnInvalid.Name = "rbtnInvalid";
+            rbtnInvalid.Size = new Size(17, 16);
+            rbtnInvalid.TabIndex = 7;
+            rbtnInvalid.TabStop = true;
+            rbtnInvalid.UseVisualStyleBackColor = true;
             // 
-            // DataValid
+            // rbtnValid
             // 
-            DataValid.AutoSize = true;
-            DataValid.Location = new Point(512, 251);
-            DataValid.Name = "DataValid";
-            DataValid.Size = new Size(17, 16);
-            DataValid.TabIndex = 6;
-            DataValid.TabStop = true;
-            DataValid.UseVisualStyleBackColor = true;
+            rbtnValid.AutoSize = true;
+            rbtnValid.Location = new Point(512, 251);
+            rbtnValid.Name = "rbtnValid";
+            rbtnValid.Size = new Size(17, 16);
+            rbtnValid.TabIndex = 6;
+            rbtnValid.TabStop = true;
+            rbtnValid.UseVisualStyleBackColor = true;
             // 
             // tbxNamaLomba
             // 
             tbxNamaLomba.BorderStyle = BorderStyle.None;
             tbxNamaLomba.Location = new Point(404, 419);
             tbxNamaLomba.Name = "tbxNamaLomba";
+            tbxNamaLomba.ReadOnly = true;
             tbxNamaLomba.Size = new Size(483, 20);
             tbxNamaLomba.TabIndex = 9;
             // 
@@ -81,6 +82,7 @@
             textBox1.BorderStyle = BorderStyle.None;
             textBox1.Location = new Point(959, 419);
             textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
             textBox1.Size = new Size(483, 20);
             textBox1.TabIndex = 10;
             // 
@@ -89,6 +91,7 @@
             textBox2.BorderStyle = BorderStyle.None;
             textBox2.Location = new Point(959, 516);
             textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
             textBox2.Size = new Size(483, 20);
             textBox2.TabIndex = 12;
             // 
@@ -97,6 +100,7 @@
             textBox3.BorderStyle = BorderStyle.None;
             textBox3.Location = new Point(404, 516);
             textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
             textBox3.Size = new Size(483, 20);
             textBox3.TabIndex = 11;
             // 
@@ -105,6 +109,7 @@
             textBox4.BorderStyle = BorderStyle.None;
             textBox4.Location = new Point(959, 613);
             textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
             textBox4.Size = new Size(483, 20);
             textBox4.TabIndex = 14;
             // 
@@ -113,6 +118,7 @@
             textBox6.BorderStyle = BorderStyle.None;
             textBox6.Location = new Point(404, 613);
             textBox6.Name = "textBox6";
+            textBox6.ReadOnly = true;
             textBox6.Size = new Size(483, 20);
             textBox6.TabIndex = 13;
             // 
@@ -129,8 +135,8 @@
             Controls.Add(textBox1);
             Controls.Add(tbxNamaLomba);
             Controls.Add(textBox5);
-            Controls.Add(DataInvalid);
-            Controls.Add(DataValid);
+            Controls.Add(rbtnInvalid);
+            Controls.Add(rbtnValid);
             Name = "Form_AchievementValidation_Admin";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form_AchievementValidation_Admin";
@@ -141,8 +147,8 @@
         #endregion
 
         private TextBox textBox5;
-        private RadioButton DataInvalid;
-        private RadioButton DataValid;
+        private RadioButton rbtnInvalid;
+        private RadioButton rbtnValid;
         private TextBox tbxNamaLomba;
         private TextBox textBox1;
         private TextBox textBox2;
